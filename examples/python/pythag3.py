ce75e22b89e1dff5ac94e77a33df608b1eb974ef
Mike Davis <mi.davis@sap.com>
2013-08-11 17:30:50 -0700

    Convert to python3.

--- examples/python/pythag3.py
+++ examples/python/pythag3.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 from math import floor, sqrt
 from mymath import gcd
 import itertools
 
 def triples( max ) :
-    for k in xrange( 1, 1+max ) :
+    for k in range( 1, 1+max ) :
         imax = int( floor( sqrt( k*k/2.0 ) ) )
-        for i in xrange( 1, 1+imax ) :
+        for i in range( 1, 1+imax ) :
             j = sqrt( k*k - i*i )
             if abs( floor( j ) - j ) < 0.000001 and 1 == gcd( i, int( j ) ) :
                 yield ( i, int( j ), k )
@@ -15,7 +15,7 @@ def triples( max ) :
 
 
 if __name__ == '__main__' :
-    print list( triples( 85 ) )
+    print(list( triples( 85 ) ))
     r = 0
     m = (0,0,0)
     for x in triples( 10000 ) :
@@ -24,6 +24,6 @@ if __name__ == '__main__' :
         if t > r :
             r = t
             m = x
-            print m
-    print m
+            print (m)
+    print (m)
     
