431134c62dc040597e1d0d174920679c6035700e
Mike Davis <mi.davis@sap.com>
2013-08-11 17:29:02 -0700

    Initial commit of python2 program to print pythagorean triples.

---
+++ examples/python/pythag3.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from math import floor, sqrt
+from mymath import gcd
+import itertools
+
+def triples( max ) :
+    for k in xrange( 1, 1+max ) :
+        imax = int( floor( sqrt( k*k/2.0 ) ) )
+        for i in xrange( 1, 1+imax ) :
+            j = sqrt( k*k - i*i )
+            if abs( floor( j ) - j ) < 0.000001 and 1 == gcd( i, int( j ) ) :
+                yield ( i, int( j ), k )
+            
+
+
+if __name__ == '__main__' :
+    print list( triples( 85 ) )
+    r = 0
+    m = (0,0,0)
+    for x in triples( 10000 ) :
+        i,j,k = x
+        t = (1.0 * i) / j
+        if t > r :
+            r = t
+            m = x
+            print m
+    print m
+    
