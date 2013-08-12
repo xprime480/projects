03964a354ccef5559e08f7fdd91ba62614b0b60d
Mike Davis <mi.davis@sap.com>
2013-08-11 17:20:25 -0700

    Conversion to python3 usage.

--- examples/python/rotate.py
+++ examples/python/rotate.py
@@ -3,9 +3,8 @@
 import string
 
 def decode(str, rotate) :
-    translator =  string.maketrans(string.lowercase,
-                                   string.lowercase[rotate:] +
-                                   string.lowercase[:rotate])
+    lc = bytes(string.ascii_lowercase, encoding='ascii')
+    translator =  bytes.maketrans(lc, lc[rotate:] + lc[:rotate])
     return str.translate(translator)
 
 
@@ -13,6 +12,6 @@ if '__main__' == __name__ :
     coded = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""
 
 
-    print decode(coded, 2)
-    print
-    print decode('map',2)
+    print(decode(coded, 2))
+    print()
+    print(decode('map',2))
