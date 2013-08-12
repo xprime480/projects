976d2ccaf121791c67f094b467ccbdc431f7a4e2
Mike Davis <mi.davis@sap.com>
2013-08-12 15:36:53 -0700

    Modified for python3 compatibility.

--- examples/python/wt.py
+++ examples/python/wt.py
@@ -12,14 +12,14 @@ def null_factory():
     return empty()
 
 def quitter():
-    for n in xrange(TOTALSWITCHES/NUMTHREADS):
+    for n in range(int(TOTALSWITCHES/NUMTHREADS)):
         yield None
 
 def scheduler():
     global threads
     try:
         while 1:
-            for thread in threads: thread.next()
+            for thread in threads: next(thread)
     except StopIteration:
         pass
 
@@ -29,6 +29,6 @@ if __name__ == "__main__":
     threads.append(quitter())
     starttime = time.clock()
     scheduler()
-    print "TOTAL TIME:    ", time.clock()-starttime
-    print "TOTAL SWITCHES:", TOTALSWITCHES
-    print "TOTAL THREADS: ", NUMTHREADS
+    print("TOTAL TIME:    ", time.clock()-starttime)
+    print("TOTAL SWITCHES:", TOTALSWITCHES)
+    print("TOTAL THREADS: ", NUMTHREADS)
