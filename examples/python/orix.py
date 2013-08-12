a035851335a4456760ac5d30e7990e21a57c1e69
Mike Davis <mi.davis@sap.com>
2013-08-12 15:51:08 -0700

    Demo of __getattr__(), etc.

---
+++ examples/python/orix.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+class TestNewStyle(object) :
+
+    allowable_attrs = ['catfish', 'banana', 'spam']
+
+    def __getattr__(self, attr) :
+        attr = attr.lower()
+        if attr in self.allowable_attrs :
+            if 'spam' == attr :
+                return 'Yum'
+            return attr
+        return None
+
+    def __setattr__(self, attr, value) :
+        pass
+
+    def __delattr__(self, attr) :
+        pass
+
+
+Q = TestNewStyle()
+print Q.catfish
+print Q.hotdog
+print Q.spam
+print Q.Spam
+TestNewStyle.allowable_attrs.append('hotdog')
+print Q.HotDog
+
