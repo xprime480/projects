0ee9e9a4257aea0f4b80a68cfbc976404d51fe0e
Mike Davis <mi.davis@sap.com>
2013-08-13 11:14:52 -0700

    Not really sure of the point of this file.
    It incorporates getpid(), casting to builtin types, and casting to
    class types.

---
+++ examples/cpp/boing.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+
+using namespace std;
+
+struct Q
+{
+  int x;
+};
+
+struct Z
+{
+  operator bool() const		// can cast to elementary types
+  {
+    return x == 0;
+  }
+
+  operator Q() const		// can cast to class types
+  {
+    Q q;
+    q.x = x+1;
+    return q;
+  }
+
+  int x;
+};
+
+
+Z f()
+{
+  Z z;
+  z.x = (getpid() % 2);
+  // return z;
+}
+
+int main(int argc, char ** argv)
+{
+  if ( f() ) {
+    cerr << "yes" << endl;
+  }
+  else {
+    cerr << "no" << endl;
+  }
+
+  Q q = f();
+  cerr << q.x << endl;
+}
+
+
