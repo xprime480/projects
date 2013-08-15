4dd08326b56e1a1b361456fbe1ba69638825437e
Mike Davis <mi.davis@sap.com>
2013-08-15 15:10:04 -0700

    Solidify the generic build.

---
+++ examples/cpp/generic.mk
@@ -0,0 +1,22 @@
+# GENERIC makefile
+
+all : $(MYEXE)
+
+OBJS      = $(patsubst %.cpp,%.o,$(wildcard *.cpp))
+CPP       = g++
+CPP_FLAGS = -std=c++11 -Wall -Werror -g
+
+include $(BASEDIR)/mk_deps.mk
+
+$(OBJS) : %.o : %.cpp
+	$(CPP) $(CPP_FLAGS) -c  -g -o $@ $<
+
+$(MYEXE): $(DEPS) $(OBJS)
+	$(CPP) -o $(MYEXE) $(OBJS)
+
+test : $(MYEXE)
+	./$(MYEXE) $(TEST_ARGS)
+
+clean :
+	-rm *.o *.exe *~ *.d
+
