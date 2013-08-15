3a8c143ceee2cab7de843c962690eeebb78a38aa
Mike Davis <mi.davis@sap.com>
2013-08-15 15:32:59 -0700

    More generic makefile enhancements.
        Add link flags
        Additonal arguments for clean targt.

--- examples/cpp/generic.mk
+++ examples/cpp/generic.mk
@@ -2,17 +2,19 @@
 
 all : $(MYEXE)
 
-OBJS      = $(patsubst %.cpp,%.o,$(wildcard *.cpp))
-CPP       = g++
-CPP_FLAGS = -std=c++11 -Wall -Werror -g
+BASEDIR    ?= .
+OBJS       ?= $(patsubst %.cpp,%.o,$(wildcard *.cpp))
+CPP        ?= g++
+CPP_FLAGS  ?= -std=c++11 -Wall -Werror -g
+LINK_FLAGS ?= -L$(BASEDIR)/lib
 
 include $(BASEDIR)/mk_deps.mk
 
 $(OBJS) : %.o : %.cpp
-	$(CPP) $(CPP_FLAGS) -c  -g -o $@ $<
+	$(CPP) $(CPP_FLAGS) -c -o $@ $<
 
 $(MYEXE): $(DEPS) $(OBJS)
-	$(CPP) -o $(MYEXE) $(OBJS)
+	$(CPP) $(CPP_FLAGS) $(LINK_FLAGS) -o $(MYEXE) $(OBJS) $(EXTRA_LIBS)
 
 test : $(MYEXE)
 	./$(MYEXE) $(TEST_ARGS)
