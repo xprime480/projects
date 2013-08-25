c32abaddc5783916248cfcce9f27bc9ec0fe2b37
Mike Davis <mi.davis@sap.com>
2013-08-25 13:16:29 -0700

    Updated to use the standard build methods.
    	Include some extra tests.

---
+++ examples/math/newton/exe.mk
@@ -0,0 +1,5 @@
+OBJS         = $(MYEXE).o
+BASEDIR      = $(shell pwd)/../../cpp
+
+include $(BASEDIR)/generic.mk
+
--- examples/math/newton/newton_test.cpp
+++ examples/math/newton/newton_test.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstdlib>
 
 #include "Newton.h"
 #include "Function.h"
