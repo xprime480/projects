ae62b43f36cb00dc16e91006379e35c6aa8e19c8
Mike Davis <mi.davis@sap.com>
2013-09-03 11:20:37 -0700

    Add functions to convert between probabilities and decibels of information.

--- examples/math/logit.py
+++ examples/math/logit.py
@@ -11,11 +11,20 @@ def p2logit(p) :
     """Convert a probability to a logit."""
     return math.log(p/(1-p))
 
+def p2db(p) :
+    """Convert a probability to a decibel."""
+    return 10.0*math.log(p/(1-p), 10)
+
+def db2p(db) :
+    """Convert a decibel to a probability."""
+    x = 10**(db/10.0)
+    return x/(1+x)
+
 def main() :
     """Execute some tests."""
 
     for x in range(-10,11) :
-        print (logit2p(.25 * x))
+        print (logit2p(.5 * x))
 
     print (logit2p(p2logit(0.1)))
 
