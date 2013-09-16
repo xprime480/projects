aed15d4941dc47be37c4098f0483289914ef56c2
Mike Davis <mi.davis@sap.com>
2013-09-16 15:59:49 -0700

    Write results to file.

--- tools/rapper/newest.sql
+++ tools/rapper/newest.sql
@@ -2,22 +2,12 @@
 -- Select never played songs.
 --
 
-SELECT 
-	Id,
-	Artist
-FROM
-	(
-	SELECT
-		track_id        AS 'Id', 
-		track_artist_id AS 'Artist',
-		random()        AS 'Random'
-	FROM
-		track t
-	WHERE 
-		in_library = 1
-	AND
-		last_played IS NULL
-	)
-ORDER BY
-	Random
+.width 48 20
+.output unheard.txt
+select track_name, date_added
+  from track
+ where play_count is null
+ order by date_added desc
+ limit 100
 ;
+.output stdout
