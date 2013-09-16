6ebd6cd698c2247db8ffd55b257d08569f52a5b7
Mike Davis <mi.davis@sap.com>
2013-09-16 15:18:36 -0700

    Select 5 star songs.

---
+++ tools/rapper/five-star.sql
@@ -0,0 +1,28 @@
+--
+-- Select 5 star songs that are in the library and haven't been 
+-- played in at least 1 day
+--
+SELECT 
+	Id,
+	Artist
+FROM
+	(
+	SELECT
+		track_id        AS 'Id', 
+		track_artist_id AS 'Artist',
+		random()        AS 'Random'
+	FROM
+		track t
+	INNER JOIN
+		ratings r
+	ON
+		t.track_id = r.ratings_local_track_id
+	WHERE 
+		(julianday('now', 'localtime', 'start of day') - julianday(substr(t.last_played, 1, 4) || '-' || substr(t.last_played, 5, 2) || '-' || substr(t.last_played,7,2), 'localtime', 'start of day')) > 1
+	AND
+		in_library = 1
+	AND
+		r.ratings_value = 5
+	)
+ORDER BY
+	Random
