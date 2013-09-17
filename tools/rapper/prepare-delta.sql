445e4e949194a1c2c77eafad4aa832915f2f8c35
Mike Davis <mi.davis@sap.com>
2013-09-17 09:11:10 -0700

    Add comments and update schema names.

--- tools/rapper/prepare-delta.sql
+++ tools/rapper/prepare-delta.sql
@@ -5,38 +5,44 @@
 .mode insert track
 .output new_tracks.sql
 
+--
+-- Get tracks which are new
+--
 select *
-  from may.track t
+  from merged.track t
  where t.in_library = 1
    and t.rhapsody_id not in 
 		 (
  		   select rhapsody_id
- 			  from june.track
+ 			  from gold.track
 		 )
    and t.track_id not in
 		 (
 		   select track_id
-			  from june.track
+			  from gold.track
 		 )
 ;
 
 .mode insert ratings
 .output new_ratings.sql
 
+--
+-- Get any ratings for new tracks.
+--
 select r.*
-  from may.ratings r,
-  		 may.track t
+  from merged.ratings r,
+  		 merged.track t
  where r.ratings_local_track_id = t.track_id
    and t.in_library = 1
    and t.rhapsody_id not in 
 		 (
  		   select rhapsody_id
- 			  from june.track
+ 			  from gold.track
 		 )
    and t.track_id not in
 		 (
 		   select track_id
-			  from june.track
+			  from gold.track
 		 )
 ;
 
