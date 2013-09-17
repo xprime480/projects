a7ca4e6821d681684033662fb387f99390866f0b
Mike Davis <mi.davis@sap.com>
2013-09-17 09:10:22 -0700

    Check for clashing ratings IDs.

--- tools/rapper/mismatch.sql
+++ tools/rapper/mismatch.sql
@@ -1,9 +1,9 @@
 --
 -- Count instances of same songs with different local track ids.
 --
-select count(*)
-  from june.track a,
-       may.track b
+select a.rhapsody_id, a.track_id, b.track_id, a.track_name, b.track_name
+  from gold.track a,
+       merged.track b
  where a.rhapsody_id = b.rhapsody_id
    and a.track_id <> b.track_id
 ;
@@ -12,8 +12,21 @@ select count(*)
 -- Count instances of track id collisions between different songs.
 --
 select count(*)
-  from june.track a,
-       may.track b
+  from gold.track a,
+       merged.track b
  where a.rhapsody_id <> b.rhapsody_id
    and a.track_id = b.track_id
 ;
+
+--
+-- Count instances of rating id collisions between different songs.
+--
+select b.ratings_id
+  from gold.ratings a,
+       merged.ratings b
+ where a.ratings_local_track_id <> b.ratings_local_track_id
+   and a.ratings_id = b.ratings_id
+;
+
+
+
