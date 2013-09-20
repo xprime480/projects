976ac96f5a2291cba7f28da0fc0e1243b57dac56
Mike Davis <mi.davis@sap.com>
2013-09-20 15:41:30 -0700

    Improve efficiency by reducing the number of processes created.

--- tools/gitutils/fixmode.py
+++ tools/gitutils/fixmode.py
@@ -2,41 +2,68 @@
 
 """Git Checkout files that only have their mode spuriously changed."""
 
+import os
 import re
 import subprocess
+import sys
 
-def check_diff(path) :
+def find_git_root() :
+    """Find the directory which is the root of the current git tree."""
+
+    curr  = os.getcwd()
+    last  = curr + os.sep + 'just something extra'
+
+    while curr != last :
+        if '.git' in os.listdir(curr) :
+            return bytes(curr, 'ascii')
+        last = curr
+        curr = os.path.abspath(last + os.sep + os.pardir)
+
+    if curr == last :
+        raise Exception('Reached the root, no git directory apparant.')
+
+def get_output_for_cmd(cmd, paths=[]) :
+    """Run a command and return output."""
+
+    temp = cmd[:]
+    temp.extend(paths)
+    proc = subprocess.Popen(temp, stdout=subprocess.PIPE)
+    out, err = proc.communicate()
+    return out
+
+def update(paths) :
+    """Perform a git checkout to fix the bad mode."""
+
+    out = get_output_for_cmd(['git', 'checkout'], paths)
+
+
+def filter_for_mode_changes(paths) :
     """Check if the only diff in a file is the mode."""
 
-    proc = subprocess.Popen(
-        ['git', 'diff', path], 
-        stdout=subprocess.PIPE
-    )
-    out,err  = proc.communicate()
-    lines    = out.split(b'\n')
-    if ( len(lines) == 4 and 
-         lines[1].startswith(b'old mode ') and 
-         lines[2].startswith(b'new mode ') ) :
-        print('updating', path, 'because of wonky mode change.')
-        p2 = subprocess.Popen(
-            ['git', 'checkout', path], 
-            stdout=subprocess.PIPE
-        )
-        p2.communicate()
+    pattern = b'^diff\\s+--git\\s+a.*\\s+b/(.*)\nold mode \\d+\nnew mode \\d+$'
+    out     = get_output_for_cmd(['git', 'diff'], paths)
+    mpaths  = [m.group(1) for m in re.finditer(pattern, out, re.MULTILINE)]
+
+    paths[:] = mpaths
 
 def main() :
     """Find all files that have spurious mode changes & fix them."""
 
-    regex = re.compile(b'.*modified:\\s+(.*)')
+    pattern = b'.*modified:\\s+(.*)'
+    out     = get_output_for_cmd(['git', 'status'])
 
-    proc     = subprocess.Popen(['git', 'status'], stdout=subprocess.PIPE)
-    out,err  = proc.communicate()
-    lines    = out.split(b'\n')
-    for line in lines :
-        match = regex.match(line)
-        if match :
-            check_diff(match.group(1))
+    paths   = [m.group(1) for m in re.finditer(pattern, out, re.MULTILINE)]
+
+    try :
+        root    = find_git_root()
+    except Exception as e :
+        print(e)
+        sys.exit(1)
+
+    filter_for_mode_changes(paths)
+    sep = bytes(os.sep, 'ascii')
+    update([root + sep + p for p in paths])
 
 if __name__ == '__main__' :
     main()
-    
+
