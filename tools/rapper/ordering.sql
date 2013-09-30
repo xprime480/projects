1a833d820c7bb9797e4dc60c40dd2476a9b187c7
Mike Davis <mi.davis@sap.com>
2013-09-30 09:22:56 -0700

    Select the current playlist.

---
+++ tools/rapper/ordering.sql
@@ -0,0 +1,12 @@
+.output ordering.txt
+
+.width 8 25 25
+
+SELECT trim(comments) AS ordering, artist_name, track_name
+  FROM track
+ WHERE comments IS NOT NULL
+   AND length(comments) > 0
+ ORDER BY comments DESC
+;
+
+.output stdout
