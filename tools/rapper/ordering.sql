ba821c7ab430e5c22e81783763a19bf8222f9b0c
Mike Davis <mi.davis@sap.com>
2013-10-07 11:06:52 -0700

    Restrict query to comments in library.

--- tools/rapper/ordering.sql
+++ tools/rapper/ordering.sql
@@ -6,6 +6,7 @@ SELECT trim(comments) AS ordering, artist_name, track_name
   FROM track
  WHERE comments IS NOT NULL
    AND length(comments) > 0
+   AND in_library = 1
  ORDER BY comments DESC
 ;
 
