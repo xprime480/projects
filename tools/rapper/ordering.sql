4e23f4a0ab2d87e9f004377360df1ae4a3f4d83b
Mike Davis <mi.davis@sap.com>
2013-10-16 10:53:52 -0700

    Eliminate use of temporary table.
    Rename some columns.
    Rectify name of output file.

--- tools/rapper/ordering.sql
+++ tools/rapper/ordering.sql
@@ -1,43 +1,37 @@
-
-CREATE TABLE temp_report
-AS
-SELECT 0 AS rating, 
-		 trim(comments) AS ordering, 
-		 artist_name, 
-		 track_name
-  FROM track
- WHERE comments IS NOT NULL
-   AND length(comments) > 0
-   AND in_library = 1
-   AND track_id NOT IN
-(
-SELECT ratings_local_track_id
-  FROM ratings
-)
-;
-
-INSERT INTO temp_report
-SELECT r.ratings_value, 
-		 trim(t.comments) AS ordering, 
-		 t.artist_name, 
-		 t.track_name
-  FROM track t,
-  		 ratings r       
- WHERE t.comments IS NOT NULL
-   AND length(t.comments) > 0
-   AND t.in_library = 1
-   AND t.track_id = r.ratings_local_track_id
-;
-
-.output o2.txt
+.output ordering.txt
 
 .width 8 8 25 25
 
-SELECT rating, ordering, artist_name, track_name
-  FROM temp_report
+SELECT *
+  FROM (
+       SELECT 0 AS rating, 
+              trim(comments) AS ordering, 
+              artist_name AS artist, 
+              track_name AS track
+         FROM track
+        WHERE comments IS NOT NULL
+          AND length(comments) > 0
+          AND in_library = 1
+          AND track_id NOT IN
+              (
+              SELECT ratings_local_track_id
+                FROM ratings
+              )
+
+       UNION
+
+       SELECT r.ratings_value AS rating, 
+              trim(t.comments) AS ordering, 
+              t.artist_name AS artist, 
+              t.track_name AS track
+         FROM track t,
+              ratings r       
+        WHERE t.comments IS NOT NULL
+          AND length(t.comments) > 0
+          AND t.in_library = 1
+          AND t.track_id = r.ratings_local_track_id
+       )
  ORDER BY ordering desc
 ;
-
-DROP TABLE temp_report;
 
 .output stdout
