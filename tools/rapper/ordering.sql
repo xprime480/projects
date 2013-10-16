a6ee26dd929cb64b51ebd10cdf567f55827c7615
Mike Davis <mi.davis@sap.com>
2013-10-16 10:45:07 -0700

    Add ratings to the output of the report.

--- tools/rapper/ordering.sql
+++ tools/rapper/ordering.sql
@@ -1,13 +1,43 @@
-.output ordering.txt
 
-.width 8 25 25
-
-SELECT trim(comments) AS ordering, artist_name, track_name
+CREATE TABLE temp_report
+AS
+SELECT 0 AS rating, 
+		 trim(comments) AS ordering, 
+		 artist_name, 
+		 track_name
   FROM track
  WHERE comments IS NOT NULL
    AND length(comments) > 0
    AND in_library = 1
- ORDER BY comments DESC
+   AND track_id NOT IN
+(
+SELECT ratings_local_track_id
+  FROM ratings
+)
 ;
+
+INSERT INTO temp_report
+SELECT r.ratings_value, 
+		 trim(t.comments) AS ordering, 
+		 t.artist_name, 
+		 t.track_name
+  FROM track t,
+  		 ratings r       
+ WHERE t.comments IS NOT NULL
+   AND length(t.comments) > 0
+   AND t.in_library = 1
+   AND t.track_id = r.ratings_local_track_id
+;
+
+.output o2.txt
+
+.width 8 8 25 25
+
+SELECT rating, ordering, artist_name, track_name
+  FROM temp_report
+ ORDER BY ordering desc
+;
+
+DROP TABLE temp_report;
 
 .output stdout
