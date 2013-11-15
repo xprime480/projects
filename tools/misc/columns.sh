d39123ea25a93717485e18f24ba97f0f53b294d2
Mike Davis <mi.davis@sap.com>
2013-11-15 11:14:50 -0700

    Take input files from command line, write to output files in working directory.

--- tools/misc/columns.sh
+++ tools/misc/columns.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/bash
 
+#
+# names of CSV files read from the command line.
+#
+# names converted to output files according to scheme
+#
+# input/directory/path/base.csv -> columns.input.directory.path.base.txt 
+#
+function report_columns () {
+    input_file=${1}
+    base_file_name=`basename ${input_file} .csv`
+    directory=`dirname ${input_file}`
+    output_infix=`echo ${directory} | tr / .`
+    output_file=columns${output_infix}.${base_file_name}.txt
+
+    head -1 ${input_file} | tr ',' '\012' | nl -ba > ${output_file}
+
+}
+
+while [ $# -gt 0 ]; do
+    report_columns ${1}
+    shift
+done
+
+exit 0
+
 for x in `ls *.csv`; do
     base=`basename $x .csv`
     text=${base}.txt
-    head -1 ${x} | tr ',' '\012' | nl -ba > ${text}
 done
 
