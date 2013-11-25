c0f586c73af176041bcc6da7e98cae8f9338deaa
Mike Davis <mi.davis@sap.com>
2013-11-25 13:48:26 -0700

    Refactor certain selectors for general usability.

--- lib/python/selectors.py
+++ lib/python/selectors.py
@@ -10,10 +10,35 @@ class NamedSelector(object) :
 
     def __call__(self, row) :
         return self.func(row)
-        
 
 def simple_column_selector(name, rename=None) :
     if rename is None :
         rename = name
 
     return NamedSelector(rename, lambda x : x[name])
+
+def get_non_nulls(ds, selector) :
+    return [v for v in [selector(d) for d in ds] if v]
+
+def _count_func(ds) :
+    return len([d for d in ds])
+
+def count_aggregator(name='Count') :
+    return NamedSelector(name, _count_func)
+
+def sum_aggregator(name, selector) :
+    def _sum_func(ds) :
+        return sum(get_non_nulls(ds, selector))
+
+    return NamedSelector(name, _sum_func)
+
+def average_aggregator(name, selector) :
+    def _avg_func(ds) :
+        vals = get_non_nulls(ds, selector)
+        if len(vals) > 0 :
+            return sum(vals) / len(vals)
+        else :
+            return 0
+
+    return NamedSelector(name, _avg_func)
+
