3a506ee3d91deaaceec0fc45812cef4d4cb5c7bf
Mike Davis <mi.davis@sap.com>
2013-12-03 09:29:53 -0700

    Create datatables with a factory to hide implementation details.

---
+++ lib/python/datatablefactory.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+import sqlite3 as lite
+import datatable
+
+class DataTableFactory(object) :
+    def __init__(self, database=':memory:') :
+        self.database = database
+        self.table_id = 0
+
+    def open(self) :
+        self.con = lite.connect(self.database)
+
+    def close(self) :
+        self.con.close()
+        self.con = None
+
+    def new_table(self, name, cols=[]) :
+        self.table_id += 1
+        name_with_id = '%s_%06d' % (name, self.table_id)
+        return datatable.DataTable(self.con, name_with_id, cols)
