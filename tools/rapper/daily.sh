702bd3d9cb0e15baf175641a35a07c5f458bbc2d
Mike Davis <mi.davis@sap.com>
2014-01-06 11:38:55 -0700

    Change generator; move post production commands to seperate file.

--- tools/rapper/daily.sh
+++ tools/rapper/daily.sh
@@ -3,13 +3,13 @@
 SRC=/cygdrive/c/Users/I809989/AppData/Roaming/Real/rhapsody/1.sqlite/local3.seb
 DST=./temp.seb
 BAK=./gold.seb
-LEN=400
+LEN=300
 
 cp ${SRC} ${DST}
 rm -f ${BAK}
 cp -p ${DST} ${BAK}
 
-SCRIPTS='./temp.py ./make_playlist2.py'
+SCRIPTS='./temp.py ./make_playlist3.py'
 
 for s in ${SCRIPTS} ; do
     if [ -x $s ]; then
@@ -19,7 +19,4 @@ done
 
 cp ${DST} ${SRC}
 
-sqlite3 -init ${HOME}/.sqliterc ${DST} <<EOF
-.read ordering.sql
-.read temp.sql
-EOF
+source finalize.sh
