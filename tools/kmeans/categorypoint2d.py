403c0381857eac559a45e51a66a0e3dcbb1b2ee7
Mike Davis <mi.davis@sap.com>
2014-02-03 13:20:16 -0700

    Refactor object type into its own class.

---
+++ tools/kmeans/categorypoint2d.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import itertools
+
+class CategoryPoint2D(object) :
+    def __init__(self) :
+        pass
+
+    @classmethod
+    def distance(cls, p1, p2) :
+        d = 0
+        for ix in [0, 1] :
+            if p1[ix] != p2[ix] :
+                d += 1
+        return d
+
+    @classmethod
+    def centroid(cls, items) :
+        c = [None, None]
+        for ix in [0, 1] :
+            vs = [i[ix] for i in items]
+            vs.sort()
+            bestv = None
+            bestl = 0
+            for x in itertools.groupby(vs) :
+                v = x[0]
+                l = len(list(x[1]))
+                if l > bestl :
+                    bestl = l
+                    bestv = v
+            c[ix] = bestv
+
+        return c
+                
