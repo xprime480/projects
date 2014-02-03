4c052b2168583b874ff03b5e9a7fc50f13f9289d
Mike Davis <mi.davis@sap.com>
2014-02-03 13:52:46 -0700

    Move data generation and loading to external file which is dynamically loaded.

--- tools/kmeans/kmodes.py
+++ tools/kmeans/kmodes.py
@@ -4,6 +4,8 @@
 # this file implements a generic k-modes classifier
 #
 
+from datareader import get_data
+
 import categorypoint2d
 import ksolver
 
@@ -11,17 +13,15 @@ class KModesSolver(ksolver.KSolver) :
     def __init__(self, count, cls) :
         super().__init__(count, cls)
 
-data_set_1 = [
-    (1,2), (1,3), (1,4), (2,5), (2,0), (3,5), (3, 7)
-]
-
 def main() :
     RANDOM_POINT_COUNT = 1000
     CLUSTER_COUNT = 3
     REPEAT_COUNT = 50
 
-    #data = gen_data_1(RANDOM_POINT_COUNT, CLUSTER_COUNT)
-    data = data_set_1[:]
+    data = get_data(
+        'datamakers.data_set_1', 
+        categorypoint2d.CategoryPoint2D
+    )
     
     solver1 = KModesSolver(CLUSTER_COUNT, categorypoint2d.CategoryPoint2D)
     results1 = ksolver.run(solver1, data, REPEAT_COUNT)
