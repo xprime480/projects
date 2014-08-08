dd00746aeaf00f2e2dcee19ac46d188746025bd3
Mike Davis <mi.davis@sap.com>
2014-08-08 14:05:25 -0700

    Refactored dedup.py

--- tools/misc/dedup.py
+++ tools/misc/dedup.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python
 
-def run(fn) :
-    d = {}
-    count = 0
-    with open(fn) as f :
-        for l in f.readlines() :
-            count += 1
-            d[l[:-1]] = count
+class Deduper(object) :
+    def __init__(self) :
+        self.lines = {}
+        self.count = 0
 
-    sl = [(v,k) for k,v in d.items()]
-    sl.sort()
-    for k,v in sl :
-        print v
+    def addLine(self, line) :
+        self.count += 1
+        self.lines[line] = self.count
+
+    def getLines(self) :
+        lineListWithIndex = [(index,line) for line,index in self.lines.items()]
+        lineListWithIndex.sort()
+        linesSortedByIndex = [line for index,line in lineListWithIndex]
+        return linesSortedByIndex
+
+class FileDeduper(object) :
+    def __init__(fileName) :
+        deduper = Deduper()
+        with open(fileName) as fileHandle :
+            for line in fileHandle.readlines() :
+                deduper.addLine(line[:-1])
+        self.lines = deduper.getLines()
+    
+    def getLines(self) :
+        return self.lines
+
+def dedupFile(fileName) :
+    deduper = FileDeduper(fileName)
+    for line in deduper.getLines :
+        print line
 
 if __name__ == '__main__' :
     import sys
-    for f in sys.argv[1:] :
-        run(f)
+    for fileName in sys.argv[1:] :
+        dedupFile(fileName)
